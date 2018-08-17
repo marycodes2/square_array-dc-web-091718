4c044b7208f97d1b8c6c1f0286a46912ea205efc
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-17 00:53:52 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |element|
-    element **= element << new_array
+    new_array >> element **= element
     puts element
   puts new_array
   new_array
