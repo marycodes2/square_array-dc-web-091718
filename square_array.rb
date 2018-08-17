2c231c4d4f32b926aa7a7ed27d38c9e1fba89927
mary.e.beliveau@gmail.com Beliveau <mary.e.beliveau@gmail.com>
2018-08-17 01:01:00 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |item|
+    new_array << item ** 2
+    puts item
+  end
+  puts new_array
+  return new_array
 end
+
+
+#basket.each do |apple|
+#    puts "Taking out #{apple}"
+#end
+list = [1, 2, 3]
+square_array(list)
